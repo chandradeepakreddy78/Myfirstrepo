dbfa35fbddc6d605d0e1cc842911d35dd750fd66
deepak <deepak@flm>
2025-08-01 10:01:26 +0000

    added java code here

--- app.java
+++ app.java
@@ -4,3 +4,16 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+public class Main {
+  public static void main(String[] args) {
+    int countdown = 3;
+
+    while (countdown > 0) {
+      System.out.println(countdown);
+      countdown--;
+    }
+
+    System.out.println("Happy New Year!!");
+  }
+}
+
